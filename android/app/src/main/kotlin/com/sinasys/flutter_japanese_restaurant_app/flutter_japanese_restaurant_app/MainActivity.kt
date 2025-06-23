a681e2374ff4cc60350f7740dad5233c08bd5fa9
Pedro Ferraz <pepito.ferraz@gmail.com>
2025-06-23 18:57:11 -0300

    Limpando dados de tentativa de build

--- android/app/src/main/kotlin/com/sinasys/flutter_japanese_restaurant_app/flutter_japanese_restaurant_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sinasys.flutter_japanese_restaurant_app.flutter_japanese_restaurant_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
